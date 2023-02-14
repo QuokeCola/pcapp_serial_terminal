1d59cc3f781408ad1a23007007e60869551a73c9
QuokeCola <quokecola@icloud.com>
2023-02-14 02:25:22 -0500

    Revised serialIF.

--- src/main/kotlin/SerialInterface.kt
+++ src/main/kotlin/SerialInterface.kt
@@ -1,22 +1,36 @@
 @file:Suppress("PrivatePropertyName", "PropertyName")
 
 import com.fazecast.jSerialComm.SerialPort
+import com.fazecast.jSerialComm.SerialPortEvent
+
+import com.fazecast.jSerialComm.SerialPortMessageListener
 
 
-class SerialInterface: Thread() {
-    private val BUFFER_SIZE: Int = 10000                  // MAXIMUM LINES THE BUFFER COULD STORE
+
+
+/**
+ * @author Chen Qian
+ * @sample
+ * SerialInterface()
+ * val port = SerialInterface().get_ports()[0]
+ * SerialInterface().set_port(port)
+ * SerialInterface().start()
+ */
+class SerialInterface {
+    private val RX_LINE_SIZE: Int = 10000            // Maximum number of lines could store.
+    private val BUFFER_STR_SIZE:  Int = 200          // Maximum number of chars each line could store.
     private var port : SerialPort? = null            // The serial port
     private var input_stream  = port?.inputStream    // IO input  stream
     private var output_stream = port?.outputStream   // IO output stream
 
     private var buffer_string = ""
-    var buffer_line = mutableListOf<String>("WELCOME TO SERIAL TERMINAL", "AUTHOR: CHEN QIAN")
+    var rx_lines = mutableListOf<String>("WELCOME TO SERIAL TERMINAL", "AUTHOR: CHEN QIAN")
 
     /**
      * Initialize the SerialInterface
      */
     init {
-        System.setProperty("fazecast.jSerialComm.appid", "ChibiOS-Terminal")
+        System.setProperty("fazecast.jSerialComm.appid", "QuokeCola.Serial-Terminal")
     }
 
     /**
@@ -37,34 +51,50 @@ class SerialInterface: Thread() {
     /**
      * Start the data receive thread.
      */
-    override fun start() {
+    fun start_receive() {
         port?.openPort()
         port?.setComPortTimeouts(SerialPort.TIMEOUT_READ_SEMI_BLOCKING, 0, 0)
+        port?.setBaudRate(115200);
+        port?.setComPortTimeouts(SerialPort.TIMEOUT_NONBLOCKING, 1000, 0);
+        port?.addDataListener(MessageListener())
         input_stream = port?.inputStream
-        super.start()
+        output_stream = port?.outputStream
+    }
+
+    fun send(string: String) {
+        output_stream?.write(string.toByteArray())
     }
 
     /**
-     * Data receive thread.
+     * Data receive listener.
      */
-    override fun run() {
-        try {
-            // Added buffer string at each iteration
-            buffer_string += input_stream?.read()?.toChar()
-            // If buffer contains /r or /n, stores the lines into buffer lines and remove the first line.
-            if (buffer_string.contains("/r")||buffer_string.contains("/n")){
-                buffer_line.add(buffer_string.split("/n")[0].removeSurrounding("/r"))
-                buffer_string = buffer_string.split("/n")[1].removeSurrounding("/r")
-                if (buffer_line.size > BUFFER_SIZE) {
-                    buffer_line.drop(1);
-                }
-            }
+    private class MessageListener : SerialPortMessageListener {
+        var delimiter = "/r/n"
+        fun byteToHex(num: Byte): String {
+            val hexDigits = CharArray(2)
+            hexDigits[0] = Character.forDigit(num.toInt() shr 4 and 0xF, 16)
+            hexDigits[1] = Character.forDigit(num.toInt() and 0xF, 16)
+            return String(hexDigits)
+        }
 
-        } catch (e: Exception) {
-            // If error occurs
-            e.printStackTrace()
-            port?.closePort()
+        override fun getListeningEvents(): Int {
+            return SerialPort.LISTENING_EVENT_DATA_RECEIVED
+        }
 
+        override fun serialEvent(event: SerialPortEvent) {
+            val byteArray = event.receivedData
+            val hexStringBuffer = StringBuffer()
+            for (i in byteArray.indices) hexStringBuffer.append(byteToHex(byteArray[i]))
+            SerialInterface().rx_lines.add(hexStringBuffer.toString())
+            println("Received the following message: $hexStringBuffer")
+        }
+
+        override fun getMessageDelimiter(): ByteArray {
+            return delimiter.toByteArray()
+        }
+
+        override fun delimiterIndicatesEndOfMessage(): Boolean {
+            return false
         }
     }
 
